04b24f2f8e86ab6950ce981827482170db1bbe34
lulima1994 <96311892+lulima1994@users.noreply.github.com>
2024-08-10 22:27:52 -0300

    update components

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 Little Lemon. All rights reserved.</p>
+            <ul>
+                <li><a href="#privacy">Privacy Policy</a></li>
+                <li><a href="#terms">Terms of Service</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+            <div>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from '../images/logo.jpg';
+
+function Header() {
+    return (
+        <header>
+            <img src={logo} alt="Little Lemon Logo" />
+        </header>
+    );
+}
+
+export default Header;
